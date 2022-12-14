4ae00830013f55be2b7aa5dcc97cc7198e7b1153
Alkindi <73395247+kindi-zack@users.noreply.github.com>
2022-12-14 14:31:04 +0700

    initiall

---
+++ 3_graduate.js
@@ -0,0 +1,41 @@
+/**
+ # Graduates
+
+### NOTES
+
+- Jalankan `npm install` terlebih dahulu
+- Pada skeleton terdapat folder `__tests__`, folder ini beserta file-file di dalamnya tidak boleh diubah sama sekali.
+- untuk menjalankan test untuk memastikan solusi kamu sudah benar, jalankan command `npm test`
+
+### RESTRICTION
+
+- Tidak boleh menggunakan built-in function apapun
+
+### HINTS
+
+- Variable `nama`, `nilai` dan `absen` __tidak boleh diganti dengan variabel lain__ dan penggunaannya sesuai dengan [Directions](#directions) yang disebutkan di bawah.
+
+---
+
+## Objectives
+Mengerti Cara Menggunakan `If-Else`
+Mengerti Logika `If-Else`
+Mengerti Cara Menggunakan Operator Evaluasi `===`, `!==`
+
+## Directions
+Diberikan 3 variable yang me-representasikan seorang murid:
+
+- nama
+- nilai
+- absen
+
+Buatlah algoritma/pseudocode dan implementasi sebuah kondisional yang menentukan apakah murid tersebut lulus atau tidak.
+
+Murid dinyatakan lulus jika:
+
+- nilainya 70 keatas
+- absennya dibawah 5 kali
+
+Tampilkan nama murid dan keterangan apakah murid tersebut 'lulus' atau 'tidak lulus'
+
+ */
